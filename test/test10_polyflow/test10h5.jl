807286e8e7d19d381370bd7b33f9497ea8f8bfb0
Jesper Frandsen <104200045+jespfra@users.noreply.github.com>
2025-09-02 19:37:50 +0200

    Remove plots in test files (#85)

--- test/test10_polyflow/test10h5.jl
+++ test/test10_polyflow/test10h5.jl
@@ -1,8 +1,4 @@
-using Test
-
-# Add the include file custom to load packages and scripts. 
-# the file is located on the main from which the file takes care of the rest. 
-include(joinpath(@__DIR__,"..", "..", "include.jl"))
+using Test, CADETJulia, CSV, DataFrames
 
 
 
@@ -21,8 +17,7 @@ solve_model(
 			alg = QNDF(autodiff=false), # Defaults to alg = QNDF(autodiff=false)
 			)
 
-using Plots
-plot(columns[1].solution_outlet[:,1])
+# plot(columns[1].solution_outlet[:,1])
 
 # Compare to analytical solution 
 using CSV,DataFrames 
--- test/test1_batch_linear/test1h5.jl
+++ test/test1_batch_linear/test1h5.jl
@@ -1,9 +1,5 @@
 
-using Test
-
-# Add the include file custom to load packages and scripts. 
-# the file is located on the main from which the file takes care of the rest. 
-include(joinpath(@__DIR__,"..", "..", "include.jl"))
+using Test, CADETJulia, CSV, DataFrames
 
 
 
@@ -22,8 +18,7 @@ solve_model(
 			alg = QNDF(autodiff=false), # Defaults to alg = QNDF(autodiff=false)
 			)
 
-using Plots
-plot(columns[1].solution_outlet[:,1])
+#plot(columns[1].solution_outlet[:,1])
 
 # Compare to analytical solution 
 using CSV,DataFrames 
