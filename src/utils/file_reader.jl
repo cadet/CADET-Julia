8d74f26859305cd95ca9ec6ee8a08485177475e0
Yuvj <yuvraj.rathore2710@gmail.com>
2025-10-22 22:25:07 +0200

    Review (cIn = 1, cOut~2)

--- src/utils/file_reader.jl
+++ src/utils/file_reader.jl
@@ -101,6 +101,28 @@ function create_units(model::Union{Dict, OrderedDict})
                 push!(columnIDs, unit_name)
                 units[unit_name] = column_instance
 
+            elseif unit_type == "RADIAL_LUMPED_RATE_MODEL_WITHOUT_PORES"
+                # Create column instance
+                # Replace the following line with your column instantiation logic
+                column_instance = rLRM(nComp = value["ncomp"], 
+				    				col_inner_radius = value["col_inner_radius"]/1.0, 
+                                    col_outer_radius = value["col_outer_radius"]/1.0, 
+					    			d_rad = value["col_dispersion"]./1.0, 
+				    	    		eps_c = value["col_porosity"]/1.0, 
+							    	c0 = value["init_c"], 
+							    	q0 = value["init_q"],
+								    # save_output = true, # defaults to true
+								    polyDeg = value["discretization"]["polyDeg"], # defaults to 4
+							    	nCells = value["discretization"]["ncol"], # defaults to 8
+                                    cross_section_area = (haskey(value, "cross_section_area") ? value["cross_section_area"] : 1.0)/1.0
+				    				)
+				column_instance.bind = get_bind(value,column_instance.bindStride)
+                push!(columns, column_instance)
+                # Assign ID and number in columns
+                push!(columnNumber, length(columnNumber)+1)
+                push!(columnIDs, unit_name)
+                units[unit_name] = column_instance
+
 			elseif unit_type == "LUMPED_RATE_MODEL_WITH_PORES"
                 # Create column instance
                 column_instance = LRMP(nComp = value["ncomp"], 
